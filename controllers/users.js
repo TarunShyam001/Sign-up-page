cb4d97487488e8a1b553cb1897d29d473e25df5d
TarunShyam001 <shyamtarun2001@gmail.com>
2024-09-04 14:07:51 +0530

    password encryption completed successfully

--- controllers/users.js
+++ controllers/users.js
@@ -28,12 +28,7 @@ exports.postAddUsers = async (req, res) => {
     const hash = await bcrypt.hash(password, saltrounds);
     const user = await Users.create({ username, email, password: hash }); 
     res.status(201).json(user);
-    
-    // bcrypt.hash(password, saltrounds, async(err, hash) => {
-    //   console.log(err);
-    //   const user = await Users.create({ username, email, password: hash}); 
-    //   return res.status(201).json(user);
-    // })
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
@@ -44,15 +39,17 @@ exports.postAddUsers = async (req, res) => {
 exports.postAddLogin = async (req, res) => {
   const {email, password} = req.body; 
   try {
-    const existingUser = await Users.findOne({where : {email}});
-    if(existingUser) {
-      if(existingUser.password != password) {
-        return res.status(401).json({ message: 'Incorrect Password' });
-      } else {
-        return res.status(201).json({message: 'Login Successfully'});
-      }
+    const user = await Users.findAll({where : {email}});
+    if(user.length > 0) {
+      bcrypt.compare(password, user[0].password, (err, response) => {
+        if(!err) {
+          res.status(201).json({message: 'Login Successfully'});
+        } else {
+          res.status(401).json({ message: 'Incorrect Password' });
+        }
+      });
     } else {
-      return res.status(404).json({ message: 'User Not Found' });
+      res.status(404).json({ message: 'User Not Found' });
     }
   } catch (error) {
     console.error(error);
